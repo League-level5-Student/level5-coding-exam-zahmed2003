5603599a3da0f252e7d9d72145525e5d2ed8bfaf
dave <dave@daves-mbp.attlocal.net>
2018-11-14 13:36:48 -0800

    initial

---
+++ src/Coding_Exam_A/CodingExamA.java
@@ -0,0 +1,25 @@
+package Coding_Exam_A;
+
+import java.awt.Color;
+
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class CodingExamA {
+	public static void main(String[] args) {
+		/*
+		 * Write a program that asks the user for three pieces of information.
+		 * 1. How many robots
+		 * 2. The color of the shapes
+		 * 3. How many sides each shape will have
+		 * 
+		 * Once the information has been collected, the program will then make the requested number of robots
+		 * each draw the requested shape in the requested color. The robots should execute at the same time so 
+		 * Threads will need to be used. For full credit, define the Thread functions with lambdas.
+		 * 
+		 * See the Coding_Exam_A_Demo.jar for an example of what the finished product should look like.
+		 */
+
+	}
+}
